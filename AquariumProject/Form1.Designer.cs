97e6aad8692cab3e8b1178692cf18cd48ccf0936
ITIvanov18 <59703243+ITIvanov18@users.noreply.github.com>
2026-01-26 22:27:00 +0200

    Basic animation engine implementation

---
+++ AquariumProject/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace AquariumProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.рибиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.добавиРибкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.рибиToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "Рибки";
+            // 
+            // рибиToolStripMenuItem
+            // 
+            this.рибиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.добавиРибкаToolStripMenuItem});
+            this.рибиToolStripMenuItem.Name = "рибиToolStripMenuItem";
+            this.рибиToolStripMenuItem.Size = new System.Drawing.Size(68, 29);
+            this.рибиToolStripMenuItem.Text = "Риби";
+            // 
+            // добавиРибкаToolStripMenuItem
+            // 
+            this.добавиРибкаToolStripMenuItem.Name = "добавиРибкаToolStripMenuItem";
+            this.добавиРибкаToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.добавиРибкаToolStripMenuItem.Text = "Добави рибка";
+            this.добавиРибкаToolStripMenuItem.Click += new System.EventHandler(this.добавиРибкаToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem рибиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem добавиРибкаToolStripMenuItem;
+    }
+}
+
---
+++ AquariumProject/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace AquariumProject
+{
+    public partial class Form1 : Form
+    {
+        List<Fish> aquariumFish = new List<Fish>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.DoubleBuffered = true;
+
+            // взима байтовете на картинката
+            byte[] imageBytes = (byte[])Properties.Resources.aquarium;
+
+            // създава поток, но НЕ слагаме 'using', за да не го затворим!
+            // този поток трябва да живее, докато приложението работи
+            MemoryStream ms = new MemoryStream(imageBytes);
+
+            this.BackgroundImage = Image.FromStream(ms);
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            foreach (var fish in aquariumFish)
+            {
+                fish.Move(this.ClientSize.Width);
+            }
+            Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            foreach (var fish in aquariumFish)
+            {
+                Image originalImg = fish.GetImage();
+
+                Image imgToDraw = originalImg;
+                bool isRotated = false; // флаг дали е създадена нова картинка
+
+                if (fish.SpeedX < 0)
+                {
+                    imgToDraw = new Bitmap(originalImg);
+                    imgToDraw.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                    isRotated = true;
+                }
+
+                e.Graphics.DrawImage(imgToDraw, fish.X, fish.Y, fish.Width, fish.Height);
+
+                // чисти само ако е създадена нова (обърната) картинка
+                if (isRotated)
+                {
+                    imgToDraw.Dispose();
+                }
+            }
+        }
+    }
+}
